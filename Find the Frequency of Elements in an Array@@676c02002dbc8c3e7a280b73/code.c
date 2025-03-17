6195f44551240118c8839a2aff2106223960cdb3
raghav1633 <raghav1633.becse24@chitkara.edu.in>
2025-03-17 21:04:37 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,39 +1,14 @@
-
 #include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int n;
-        scanf("%d", &n);
-            int *arr = (int *)malloc(n * sizeof(int));
-                int *freq = (int *)calloc(n, sizeof(int));
-                    int *visited = (int *)calloc(n, sizeof(int));
-                        int count = 0;
-                            for (int i = 0; i < n; i++) {
-                                    scanf("%d", &arr[i]);
-                                            visited[i] = 0;
-                                                }
-                                                    for (int i = 0; i < n; i++) {
-                                                            if (visited[i])
-                                                                        continue;
-                                                                                int cnt = 1;
-                                                                                        for (int j = i + 1; j < n; j++) {
-                                                                                                    if (arr[i] == arr[j]) {
-                                                                                                                    cnt++;
-                                                                                                                                    visited[j] = 1;
-                                                                                                                                                }
-                                                                                                                                                        }
-                                                                                                                                                                freq[count] = cnt;
-                                                                                                                                                                        arr[count] = arr[i];
-                                                                                                                                                                                count++;
-                                                                                                                                                                                    }
-                                                                                                                                                                                        for (int i = 0; i < count; i++) {
-                                                                                                                                                                                                printf("%d %d\n", arr[i], freq[i]);
-                                                                                                                                                                                                    }
-                                                                                                                                                                                                        free(arr);
-                                                                                                                                                                                                            free(freq);
-                                                                                                                                                                                                                free(visited);
-                                                                                                                                                                                                                    return 0;
-                                                                                                                                                                                                                    }
-
-                                                                                                                                                                                                                    
+int main(){
+int N;
+scanf("%d",&N);
+int arr[N];
+for(int i=0;i<N;i++){
+    int a;
+    arr[i]=scanf("%d",&a)
+}
+for(int i=0;i<N;i++){
+    
+}
+return 0;
+}
