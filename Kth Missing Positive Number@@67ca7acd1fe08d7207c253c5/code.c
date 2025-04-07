afb08b611f40901e709ab2b26b595540207e9723
raghav1633 <raghav1633.becse24@chitkara.edu.in>
2025-04-07 20:57:32 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -5,6 +5,7 @@ while(i<n){
     no_misses=arr[i]-num;
     for(int s=0;s<no_misses;s++){
         k--;num++;
+            printf("Value of i %d, value of k %d , value num %d\n",i,k,num);
         if(k==0){
             printf("%d",num);
             return 0;
